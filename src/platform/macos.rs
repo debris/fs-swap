237c1c13b92f9ffc02d106bfe5bd9ef62ae93b69
debris <marek.kotewicz@gmail.com>
2018-06-04 16:34:20 +0200

    fixed error code for renamex_np

--- src/platform/macos.rs
+++ src/platform/macos.rs
@@ -15,8 +15,10 @@ pub fn swap<A, B>(a: A, b: B) -> io::Result<()> where A: AsRef<Path>, B: AsRef<P
 	let b_path = ffi::CString::new(b.as_ref().as_os_str().as_bytes())
 		.map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
-	match unsafe { renamex_np(a_path.as_ptr(), b_path.as_ptr(), RENAME_SWAP) } {
-		0 => Ok(()),
-		code => Err(io::Error::new(io::ErrorKind::Other, format!("renamex_np failed with code: {}", code))),
+	unsafe {
+		match renamex_np(a_path.as_ptr(), b_path.as_ptr(), RENAME_SWAP) {
+			0 => Ok(()),
+			_ => Err(io::Error::new(io::ErrorKind::Other, format!("renamex_np failed with code: {}", *libc::__error()))),
+		}
 	}
 }
