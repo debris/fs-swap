7a38954aaae7e29126bd4661ade5e3219f33cd30
debris <marek.kotewicz@gmail.com>
2018-06-12 14:49:30 +0200

    add DeleteFileTransactedW on windows

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -11,7 +11,7 @@ use self::winapi::um::errhandlingapi::GetLastError;
 use self::winapi::um::fileapi::GetTempFileNameW;
 use self::winapi::um::handleapi::INVALID_HANDLE_VALUE;
 use self::winapi::um::ktmw32::{CreateTransaction, RollbackTransaction, CommitTransaction};
-use self::winapi::um::winbase::MoveFileTransactedW;
+use self::winapi::um::winbase::{MoveFileTransactedW, DeleteFileTransactedW};
 
 /// Function used to create a temporary file in given directory
 fn tmp_name_in_dir(dir: &Path) -> io::Result<OsString> {
@@ -20,8 +20,12 @@ fn tmp_name_in_dir(dir: &Path) -> io::Result<OsString> {
 	let dir: Vec<u16> = OsStr::new(dir).encode_wide().chain(Some(0)).collect();
 
 	unsafe {
-		if GetTempFileNameW(dir.as_ptr(), pre.as_ptr(), 1, out.as_mut_ptr()) != 0 {
-			Ok(OsString::from_wide(&out))
+		if GetTempFileNameW(dir.as_ptr(), pre.as_ptr(), 0, out.as_mut_ptr()) != 0 {
+			out.set_len(MAX_PATH);
+			let n = out.iter()
+				.position(|&x| x == 0)
+				.ok_or_else(|| io::Error::new(io::ErrorKind::Other, "GetTempFileNameW returned invalid path".to_string()))?;
+			Ok(OsString::from_wide(&out[..n]))
 		} else {
 			Err(io::Error::new(io::ErrorKind::Other, format!("GetTempFileNameW failed with code: {}", GetLastError())))
 		}
@@ -38,6 +42,20 @@ impl Transaction {
 				return Err(io::Error::new(io::ErrorKind::Other, format!("ktmw32::CreateTransaction failed with code: {}", GetLastError())));
 			}
 			Ok(Transaction(handle))
+		}
+	}
+
+	fn delete_file<A>(&self, a: A) -> io::Result<()> where A: AsRef<OsStr> {
+		let a: Vec<u16> = a.as_ref().encode_wide().chain(Some(0)).collect();
+
+		unsafe {
+			if DeleteFileTransactedW(a.as_ptr(), self.0) != FALSE {
+				Ok(())
+			} else {
+				let error = GetLastError();
+				self.rollback();
+				Err(io::Error::new(io::ErrorKind::Other, format!("DeleteFileTransactedW failed with code: {}", error)))
+			}
 		}
 	}
 
@@ -87,6 +105,7 @@ pub fn swap<A, B>(a: A, b: B) -> io::Result<()> where A: AsRef<Path>, B: AsRef<P
 	let tmp = tmp_name_in_dir(parent_dir)?;
 
 	let transaction = Transaction::new()?;
+	transaction.delete_file(&tmp)?;
 	transaction.move_file(&a, &tmp)?;
 	transaction.move_file(&b, &a)?;
 	transaction.move_file(&tmp, &b)?;
