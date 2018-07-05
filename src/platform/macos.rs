61c533ab0f5c25c2aea061a963273db3a7b8d431
debris <marek.kotewicz@gmail.com>
2018-07-05 11:43:26 +0200

    use libloading on osx to load renamex_np

--- src/platform/macos.rs
+++ src/platform/macos.rs
@@ -1,12 +1,17 @@
 extern crate libc;
+extern crate libloading;
 
 use std::os::unix::ffi::OsStrExt;
 use std::path::Path;
 use std::{io, ffi};
+use self::libloading::os::unix::Library;
+
+unsafe fn get_renamex_np() -> Option<libloading::os::unix::Symbol<unsafe extern fn (oldpath: *const libc::c_char, newpath: *const libc::c_char, flags: libc::c_uint) -> libc::c_int>> {
+	let lib = Library::this();
+	lib.get(b"renamex_np").ok()
+}
 
 extern "stdcall" {
-	fn renamex_np(oldpath: *const libc::c_char, newpath: *const libc::c_char, flags: libc::c_uint) -> libc::c_int;
-
 	fn exchangedata(oldpath: *const libc::c_char, newpath: *const libc::c_char, flags: libc::c_uint) -> libc::c_int;
 }
 
@@ -17,13 +22,15 @@ pub fn swap<A, B>(a: A, b: B) -> io::Result<()> where A: AsRef<Path>, B: AsRef<P
 	let b_path = ffi::CString::new(b.as_ref().as_os_str().as_bytes())?;
 
 	unsafe {
-		if renamex_np(a_path.as_ptr(), b_path.as_ptr(), RENAME_SWAP) == 0 {
-			return Ok(());
-		}
+		if let Some(renamex_np) = get_renamex_np() {
+			if renamex_np(a_path.as_ptr(), b_path.as_ptr(), RENAME_SWAP) == 0 {
+				return Ok(())
+			}
 
-		let err = *libc::__error();
-		if err != libc::ENOTSUP {
-			return Err(io::Error::new(io::ErrorKind::Other, format!("renamex_np failed with code: {}", err)));
+			let err = *libc::__error();
+			if err != libc::ENOTSUP {
+				return Err(io::Error::new(io::ErrorKind::Other, format!("renamex_np failed with code: {}", err)));
+			}
 		}
 
 		// some volumes do not support swapping
